b47a14b92b4e9fa1e87ac5f21c4f569339e44ede
Markus Tacker <m@cto.hiv>
2016-03-22 19:26:13 +0100

    Added landingpage from FTP.

---
+++ landingpage/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay: 8000, //Set AutoPlay to 3 seconds
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
