d856961e4da3efc60660e7e6652a9983a26bbfee
Markus Tacker <markus@resourceful-humans.com>
2016-10-21 16:43:42 +0200

    feat(newtoRHk): add contact me footer

--- src/js/module/navigation.js
+++ src/js/module/navigation.js
@@ -6,12 +6,18 @@ module.exports = ($, $htmlBody, window, document) => {
   $('.navbar *[href]').on('click', (ev) => {
     ev.preventDefault()
     $navbarMobile.collapse('hide')
-    const href = $(ev.target).closest('a').attr('href')
+    const a = $(ev.target).closest('a')
+    const href = a.attr('href')
+    const target = a.attr('target')
     const trg = href.split('#')
     if (trg.length === 2 && document.location.pathname === trg[0]) {
       $htmlBody.animate({scrollTop: $('#' + trg[1]).offset().top - 40}, 1000)
     } else {
-      window.location = href
+      if (target === '_blank') {
+        window.open(href)
+      } else {
+        window.location = href
+      }
     }
   })
 }
